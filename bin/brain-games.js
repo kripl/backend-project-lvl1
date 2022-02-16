63ae2197aac893e0594a57e48d8be102de876b93
k.sobol.work@gmail.com <kripl.dev.2020@gmail.com>
2022-02-16 20:52:37 +0200

    change func name to sayGreet

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { greeteng } from '../src/cli.js';
+import sayGreet from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-console.log(greeteng());
+console.log(sayGreet());
