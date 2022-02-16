5fdd0039a1b0053d13b68d6bdb9d9d8469f86873
k.sobol.work@gmail.com <kripl.dev.2020@gmail.com>
2022-02-16 20:54:01 +0200

    fix linter erorrs

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const greeteng = () => {
-    const name = readlineSync.question('May I have your name? ');
-    const greet = `Hello, ${name}!`;
-    return greet;
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
+  const greet = `Hello, ${name}!`;
+  return greet;
 };
